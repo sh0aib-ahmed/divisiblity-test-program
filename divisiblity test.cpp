0f62686efe0c2c6934c981d51b0d5918eae3f8b5
Shoaib ahmed <53125701+sh0aib-ahmed@users.noreply.github.com>
2019-10-05 18:11:30 +0530

    Add files via upload

---
+++ divisiblity test.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	int a;
+	cout<<"enter a number : ";
+	cin>>a;
+			if(a%2==0)
+			{
+			cout<<"number is divisible by 2";
+			return a;
+			
+			}
+        	if(a%3==0)
+			{
+				cout<<"number is divisible by 3";
+				return a;
+			}
+			if(a%4==0)
+			{
+				cout<<"number is divisible by 4";
+				return a;
+			}if(a%5==0)
+			{
+				cout<<"number is divisible by 5";
+				return a;
+			}if(a%6==0)
+			{
+				cout<<"number is divisible by 6";
+				return a;
+			}if(a%7==0)
+			{
+				cout<<"number is divisible by 7";
+				return a;
+			}if(a%8==0)
+			{
+				cout<<"number is divisible by 8";
+				return a;
+			}if(a%9==0)
+			{
+				cout<<"number is divisible by 9";
+				return a;
+			}
+		
+             else
+            {
+				cout<<"number is indivisible ";
+				
+			}
+	
+	return 0;
+}
